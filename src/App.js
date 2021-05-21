136d43c91ab8a0506ea49c6496fb61931df46f5c
Aftaab Dobani <1234dobani@gmail.com>
2021-05-20 21:38:20 -0400

    added aboutme

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar/Navbar"
 import Header from "./components/Header/Header"
+import AboutMe from "./components/About/About"
 
 function App() {
   return (
     <>
     <Navbar />
     <Header />
+    <AboutMe />
     </>
   );
   }
---
+++ src/components/About/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./style.css"
+
+const About = () => {
+    return (
+        <div className="container py-5">
+            <div className="row">
+                <div className=".col.lg-6 col-xm-12">
+                    <h1 className="about-heading"> About Me </h1>
+                    <p>
+                        Hello! I am Aftaab Dobani. I am currently a Full Stack Developer student at Georgia Tech Bootcamp located in Atlanta, Georgia. 
+
+                        I am obsessed with learning new things and making things better with technology. I also have a huge passion for trading stocks, crypto and building my sneaker collection.
+                    </p>
+
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default About
